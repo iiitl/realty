465444331237f3c4ad0efd2315e380c56ecfd81d
vishal18713 <lcs2023052@iiitl.ac.in>
2024-03-15 14:52:10 +0530

    Add Contract deployment and interaction in Deploy.js

--- scripts/Deploy.js
+++ scripts/Deploy.js
@@ -5,17 +5,32 @@ const tokens = (n) => {
 }
 
 async function main() {
+    const [deployer] = await ethers.getSigners(); 
 
     const RealEstate =await ethers.getContractFactory('RealEstate');
     const realEstate = await RealEstate.deploy();
     await realEstate.deployed();
+    console.log('realEstate address: ' + realEstate.address);
 
-    console.log('realEstate address' + realEstate.address);
-    console.log("Mining starting");
+    const Contract = await ethers.getContractFactory('Contract');
+    const contract = await Contract.deploy(realEstate.address, deployer.address);
+    await contract.deployed();
+    console.log('Contract address: ' + contract.address);
 
+
+    const newItemId = await realEstate.mint("metadata_uri_here");
+    await contract.list1(
+        newItemId,
+        "Amenities",
+        1000, 
+        3,    
+        "image_uri_here",
+        "Description",
+        tokens(10), 
+        newItemId 
+    );
     
-
-   
+    console.log("Mining starting"); 
 }
 
 main().catch((error) => {
