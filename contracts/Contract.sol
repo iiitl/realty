863cdd40f6d9429644b17a9e78d1e4471cfca892
NeonKazuha <tanudeshtd2005@gmail.com>
2024-03-16 22:59:15 +0530

    Fixed Bug

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 interface IERC721 {
     function transferFrom(address _from, address _to, uint256 _id);
-}
+} external;
 
 contract Contract {
     address public nftaddress;
