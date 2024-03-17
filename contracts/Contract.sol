61853ea1e952abac184d36c569f06a96b8342941
Dishant <dgarg9803@gmail.com>
2024-03-17 23:41:15 +0530

    Resolved issue #7

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -142,16 +142,21 @@ contract Contract {
     function listed(uint256 _nftID) public view returns(bool){
         return isListed[_nftID];
     }
-    event SaleCancelled(uint256 indexed nftID);
+     event SaleCancelled(uint256 indexed nftID);
     function cancelSale(uint256 _nftID) public {
       require(msg.sender == seller || msg.sender == buyer[_nftID], "Only seller or buyer can cancel the sale");
     require(isListed[_nftID], "Property is not listed for sale");
-    IERC721(nftaddress).transferFrom(address(this), seller, _nftID);
+    try IERC721(nftaddress).transferFrom(address(this), seller, _nftID) {
     isListed[_nftID] = false;
     if (msg.sender == buyer[_nftID]) {
         seller.transfer(purchasePrice[_nftID]);
     }
     emit SaleCancelled(_nftID);
+    }
+    catch (bytes memory)
+    {
+        revert("Failed to transfer NFT back to the seller");
+    }
     }
     function retprice (uint256 _nftID) public view returns (uint256) {
         return purchasePrice[_nftID];
