881269def0cdcf05a5077b8f55ced6588aaaf00f
nithincod <nithinrohitian@gmail.com>
2024-03-17 23:52:37 +0530

    completed deployment script

--- scripts/Deploy.js
+++ scripts/Deploy.js
@@ -1,3 +1,5 @@
+
+
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 const tokens = (n) => {
@@ -5,17 +7,18 @@ const tokens = (n) => {
 }
 
 async function main() {
-
+    // getting signers
     let [seller] = await ethers.getSigners();
 
     const RealEstate = await ethers.getContractFactory('RealEstate');
     const realEstate = await RealEstate.deploy();
     await realEstate.deployed();
-
+    // in console printing
     console.log('realEstate address' + realEstate.address);
 
     const Contract = await ethers.getContractFactory('Contract');
-    
+    const contract = await Contract.deploy(realEstate.address, seller.address);
+    await contract.deployed();
      
   
 }
@@ -24,5 +27,6 @@ main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
 });
+
 
   
